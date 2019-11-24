01389c4eb1af6be6b5973d41aa4d94993db0cbc4
Patrick Rodrigues <47316946+rodpa715@users.noreply.github.com>
2019-11-24 01:10:23 +0100

    set env to be development unless specified

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,8 @@
 const knex = require('knex')
 const knexConfig = require('../knexfile.js')
 
-const db = knex(knexConfig[process.env.NODE_ENV === 'production' ? 'production' : 'development'])
+const env = process.env.NODE_ENV || 'development';
+
+const db = knex(knexConfig[env])
 
 module.exports = db
