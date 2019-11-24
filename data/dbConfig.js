a7f1be34b8ba1fb873989d5deee14d59ca5678ff
Patrick Rodrigues <47316946+rodpa715@users.noreply.github.com>
2019-11-24 01:35:00 +0100

    change NODE_ENV to DB_ENV

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,7 +1,7 @@
 const knex = require('knex')
 const knexConfig = require('../knexfile.js')
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.DB_ENV || 'development';
 
 const db = knex(knexConfig[env])
 
